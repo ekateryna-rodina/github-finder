8794c1d8911141505120fa7f74394631c05fbf97
Kateryna Rodina <work.rodina@gmail.com>
2020-12-26 12:31:55 -0500

    added users repos

--- src/App.js
+++ src/App.js
@@ -1,28 +1,30 @@
 import './App.css';
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import Navbar from './components/layout/Navbar'
-import UserItem from './components/users/UserItem'
 import Users from './components/users/Users'
 import axios from 'axios'
 import Search from './components/users/Search'
 import Alert from './components/layout/Alert';
-
-
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import About from './components/pages/About';
+import User from './components/users/User'
 
 class App extends Component {
   state = {
     users: [],
     loading: false,
-    alert: null
+    alert: null,
+    user: {},
+    repos: []
 }
-
+// load users
   async componentDidMount(){
     this.setState({loading: true})
     const res = await axios.get(`https://api.github.com/users?client_id=
           ${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`)
     this.setState({users: res.data, loading: false})
   }
-
+// search users
   searchUsers = async text =>{
     this.setState({loading: true})
     const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=
@@ -38,21 +40,54 @@ class App extends Component {
     this.setState({alert: {msg: msg, type: type}})
     setTimeout(() => {this.setState({alert: null})}, 5000)
   }
+  // get single user
+  getUser = async(username) => {
+    this.setState({loading: true})
+    const res = await axios.get(`https://api.github.com/users/${username}?client_id=
+          ${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`)
+    this.setState({user: res.data, loading: false})
+  }
+  getUserRepos = async(username) => {
+    this.setState({loading: true})
+    const res = await axios.get(`https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=
+          ${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`)
+    this.setState({repos: res.data, loading: false})
+  }
    render() {
-     const {users, loading} = this.state;
+     const {users, loading, user, repos} = this.state;
      return (
+       <Router>
       <div className="App">
         <Navbar />
         <div className="container">
           <Alert alert={this.state.alert}/>
-          <Search 
-            searchUsers={this.searchUsers} 
-            clearUsers={this.clearUsers} 
-            showClear={users.length > 0}
-            showAlert={this.showAlert}/>
-          <Users users={users} loading={loading}/>
+          <Switch>
+            <Route exact path='/' render={props => (
+              <Fragment>
+                  <Search 
+                    searchUsers={this.searchUsers} 
+                    clearUsers={this.clearUsers} 
+                    showClear={users.length > 0}
+                    showAlert={this.showAlert}/>
+                  <Users users={users} loading={loading}/>
+                </Fragment>
+            )}/>
+            <Route exact path='/about' component={About}></Route>
+            <Route exact path='/user/:login' render={props => (
+              <User 
+                {...props} 
+                getUser={this.getUser} 
+                user={user} 
+                getUserRepos={this.getUserRepos} 
+                loading={loading} 
+                repos={repos}>
+              </User>
+            )}>
+            </Route>
+          </Switch>
         </div>
       </div>
+      </Router>
      )};
 } 
 export default App;
--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+import {Link} from 'react-router-dom';
 
 const Navbar = ({icon, title}) =>  {
     return (
         <nav className='navbar bg-primary'>
             <h4><i className={icon}></i> {title}</h4> 
+            <ul>
+                <li><Link to='/'>Home</Link></li>
+                <li><Link to='/about'>About</Link></li>
+            </ul>
         </nav>
     )
 }
