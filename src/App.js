27952cbe5f362f224f5b9f4e3ec0e964276ce285
Kateryna Rodina <work.rodina@gmail.com>
2020-12-28 16:07:46 -0500

    added home component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import './App.css';
-import React, { useState, Fragment } from 'react';
+import React from 'react';
 import Navbar from './components/layout/Navbar'
-import Users from './components/users/Users'
-import Search from './components/users/Search'
 import Alert from './components/layout/Alert';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import About from './components/pages/About';
 import User from './components/users/User';
 import GithubState from './context/github/githubState';
 import AlertState from './context/alert/alertState';
+import { Home } from './components/pages/Home';
+import { NotFound } from './components/pages/NotFound';
 
 const App = () => {
      return (
@@ -20,15 +20,10 @@ const App = () => {
         <div className="container">
           <Alert/>
           <Switch>
-            <Route exact path='/' render={props => (
-              <Fragment>
-                  <Search/>
-                  <Users/>
-                </Fragment>
-            )}/>
-            <Route exact path='/about' component={About}></Route>
-            <Route exact path='/user/:login' component={User}>
-            </Route>
+            <Route exact path='/' component={Home}/>
+            <Route exact path='/about' component={About} />
+            <Route exact path='/user/:login' component={User} />
+            <Route component={NotFound}></Route>
           </Switch>
         </div>
       </div>
